2d8f1896470e7cf830be507813807e244d3b2d81
mehkey <87057470+mehkey@users.noreply.github.com>
2023-03-26 23:57:21 -0400

    Update Book.java

--- src/main/java/com/mehkey/graphql/model/Book.java
+++ src/main/java/com/mehkey/graphql/model/Book.java
@@ -1,3 +1,17 @@
+package com.mehkey.graphql.model;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -11,5 +25,6 @@ public class Book {
  
  @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.EAGER)
  @JoinColumn(name = "book_id")
-    private List<BookComment> comments = new ArrayList<>();
+ private List<BookComment> comments = new ArrayList<>();
+
 }
