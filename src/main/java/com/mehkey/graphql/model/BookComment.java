a637c0f418026a975dcfc1b329294deca4decb08
mehkey <87057470+mehkey@users.noreply.github.com>
2023-03-26 23:57:23 -0400

    Update BookComment.java

--- src/main/java/com/mehkey/graphql/model/BookComment.java
+++ src/main/java/com/mehkey/graphql/model/BookComment.java
@@ -1,3 +1,18 @@
+package com.mehkey.graphql.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
 @Entity
 public class BookComment { 
  @Id
@@ -6,7 +21,7 @@ public class BookComment {
  
  private String comentContent;
 
- public BookComments() {
+ public BookComment() {
   super();
   
  }
