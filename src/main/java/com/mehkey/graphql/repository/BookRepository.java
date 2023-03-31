ed96734dc1d369d666e0e47dd2424b62f8893089
mehkey <87057470+mehkey@users.noreply.github.com>
2023-03-30 23:50:45 -0400

    Create BookRepository.java

---
+++ src/main/java/com/mehkey/graphql/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.mehkey.graphql.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mehkey.graphql.model.Book;
+
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, String> {
+}
