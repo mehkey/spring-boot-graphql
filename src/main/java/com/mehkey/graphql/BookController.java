9eea9c3dbfda1d81ddd59e004fc38dd2cd06dd58
mehkey <87057470+mehkey@users.noreply.github.com>
2023-03-30 23:51:27 -0400

    Update BookController.java

--- src/main/java/com/mehkey/graphql/BookController.java
+++ src/main/java/com/mehkey/graphql/BookController.java
@@ -1,5 +1,9 @@
 package com.mehkey.graphql;
 
+import com.mehkey.graphql.model.Author;
+import com.mehkey.graphql.model.Book;
+import com.mehkey.graphql.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
@@ -7,13 +11,23 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) { super();
+        this.bookService = bookService;
+    }
+
     @QueryMapping
     public Book bookById(@Argument String id) {
-        return Book.getById(id);
+        //
+        //return Book.getById(id);
+        return this.bookService.getBooks(id);
     }
 
     @SchemaMapping
     public Author author(Book book) {
-        return Author.getById(book.getAuthorId());
+        return null;//Author.getById(book.getAuthorId());
     }
 }
