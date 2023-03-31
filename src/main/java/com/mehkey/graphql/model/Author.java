162ce26fc885fb18e40bafe125c6785c1bfd8f1b
mehkey <87057470+mehkey@users.noreply.github.com>
2023-03-30 23:51:20 -0400

    Create Author.java

---
+++ src/main/java/com/mehkey/graphql/model/Author.java
@@ -0,0 +1,29 @@
+package com.mehkey.graphql.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    public Author(){
+
+    }
+    public Author(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
